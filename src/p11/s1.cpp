96f0b7e7359772e5a091b0aeed5e03c893b7bafb
ATM SALEH <saleh.cse08@gmail.com>
2023-09-04 12:42:35 +0000

    Refactor: split H-index solution to multiple functions

--- src/p11/s1.cpp
+++ src/p11/s1.cpp
@@ -1,26 +1,32 @@
 #include <s1.h>
 
+namespace
+{
+    std::size_t getBucketIndex(std::size_t idx, std::size_t last)
+    {
+        return idx >= last ? last : idx;
+    }
+
+    std::size_t getHIndexFromBuckets(const std::vector<int>& buckets)
+    {
+        for (int count = 0, i = buckets.size() - 1; i >= 0; i--)
+        {
+            if (count += buckets.at(i); count >= i)
+            {
+                return i;
+            }
+        }
+        return 0;
+    }
+}
+
 int S1::hIndex(const std::vector<int>& citations) const
 {
     std::size_t ccount{citations.size()};
-    std::vector<int> buffer(ccount + 1, 0);
+    std::vector<int> buckets(ccount + 1, 0);
     for (const auto citation: citations)
     {
-        if (citation >= ccount)
-        {
-            buffer[ccount]++;
-        }
-        else
-        {
-            buffer[citation]++;
-        }
+        buckets[getBucketIndex(citation, ccount)]++;
     }
-    for (int count = 0, i = buffer.size() - 1; i >= 0; i--)
-    {
-        if (count += buffer.at(i); count >= i)
-        {
-            return i;
-        }
-    }
-    return 0;
+    return getHIndexFromBuckets(buckets);
 }
